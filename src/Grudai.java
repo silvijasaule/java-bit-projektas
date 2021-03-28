9527c3ef17dac353b6bd793536a3a8f8b3c8bded
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2021-03-28 13:15:40 +0300

    Grūdų skaičiavimas

---
+++ src/Grudai.java
@@ -0,0 +1,10 @@
+public class Grudai {
+    public static void main(String[] args) {
+        int k = 1;
+        System.out.println(k);
+        for (var i = 1; i < 64; i++) {
+            k = k * 2;
+            System.out.println(k);
+        }
+    }
+}
