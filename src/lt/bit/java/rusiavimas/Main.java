58406d169ec7d706c3854c9302b309c3f05199cc
silvijasaule <74830190+silvijasaule@users.noreply.github.com>
2021-03-31 16:07:23 +0300

    new function

---
+++ src/lt/bit/java/rusiavimas/Main.java
@@ -0,0 +1,27 @@
+package lt.bit.java.rusiavimas;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int []masyvas = {1,5,6,8,3};
+        System.out.println(Arrays.toString(masyvas));
+
+        int tuscias;
+        for (int i = 0; i < masyvas.length; i++){
+            for (int j = i+ 1; j < masyvas.length; j++) {
+                if (masyvas[i] < masyvas[j]){
+                  tuscias = masyvas[i];
+                  masyvas[i] = masyvas[j];
+                  masyvas[j] = tuscias;
+                  System.out.println(Arrays.toString(masyvas));
+                }
+            }
+
+        }
+
+    }
+
+
+}
